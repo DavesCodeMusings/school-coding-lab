aae041950d05772e6f743af0b947951992c1a6a5
David Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2024-06-09 12:08:58 -0500

    Update install_ansible.sh

--- rpi/fleet/install_ansible.sh
+++ rpi/fleet/install_ansible.sh
@@ -1,6 +1,6 @@
 echo "Checking for Ansible."
 if [ "$(command -v ansible-playbook)" == "" ]; then
-  echo "Installing the Ansible automation software package."
+  echo "Installing the Ansible automation software package..."
   sudo apt-get update && sudo apt-get install -y ansible
 else
   echo "Ansible is already installed. Nice!"
