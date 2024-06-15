251802978ab72882aedd5eec3e6fd7ebf039473d
David Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2024-06-15 08:32:01 -0500

    Doc update (#19)
    * Update fresh_install.md
    
    * Create quickstart_dev_workstation.sh
    
    * Update fresh_install.md
    
    * Update quickstart_dev_workstation.sh
    
    * Update fresh_install.md

---
+++ rpi/fleet/quickstart_dev_workstation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Download and run automation for creating a student developer workstation.
+
+check_download_error () {
+  if [ $1 != 0 ]; then
+    echo "Something went wrong. Are you connected to the internet?"
+  fi
+}
+
+wget https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/install_ansible.sh
+check_download_error $?
+
+wget https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/configure_dev_workstation.yml
+check_download_error $?
+
+bash install_ansible.sh
+
+ansible-playbook configure_dev_workstation.yml
+
+echo -n "Everything is done but the reboot. Restart now [y/N]?"
+read REPLY
+if [ "$REPLY" == "Y" ] || [ "$REPLY" == "y" ]; then
+  sudo shutdown -r now
+fi
