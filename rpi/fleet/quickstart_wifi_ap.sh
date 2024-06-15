b6153be921c3612b8b954b49d492b6ac2655e232
David Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2024-06-15 14:27:33 -0500

    Create quickstart_wifi_ap.sh [ci skip]

---
+++ rpi/fleet/quickstart_wifi_ap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Download and run automation for creating a wireless access point.
+
+check_download_error () {
+  if [ $1 != 0 ]; then
+    echo "Something went wrong. Are you connected to the internet?"
+  fi
+}
+
+wget https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/install_ansible.sh
+check_download_error $?
+
+wget https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/configure_wifi_ap.yml
+check_download_error $?
+
+bash install_ansible.sh
+
+ansible-playbook configure_wifi_ap.yml
+
+echo -n "Everything is done but the reboot. Restart now [y/N]? "
+read REPLY
+if [ "$REPLY" == "Y" ] || [ "$REPLY" == "y" ]; then
+  sudo shutdown -r now
+fi
