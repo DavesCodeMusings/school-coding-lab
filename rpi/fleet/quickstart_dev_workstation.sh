43b58b983369c2fdc5ae338694cfa8750f7a8d05
David Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2024-06-15 08:04:34 -0500

    Create quickstart_dev_workstation.sh

---
+++ rpi/fleet/quickstart_dev_workstation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Download and run automation for creating a student developer workstation.
+
+check_download_error () {
+  if [ $1 != 0 ]; then
+    echo "Something went wrong. Are you connected to the internet?"
+  fi
+}
+
+wget https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/install_ansible.sh
+check_download_error $?
+
+wget https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/configure_dev_workstation.yml
+check_download_error $?
+
+bash install_ansible.sh
+
+ansible-playbook configure_dev_workstation.yml
