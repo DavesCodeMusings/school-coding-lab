58cfcbbbdd4d5670721e2282afb3294168689744
David Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2024-06-19 19:42:42 -0500

    Update quickstart_dev_workstation.sh

--- rpi/fleet/quickstart_dev_workstation.sh
+++ rpi/fleet/quickstart_dev_workstation.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-# Download and run automation for creating a student developer workstation.
+###
+### Non-interactive configuration of Raspberry Pi workstation.
+###
 
-check_download_error () {
-  if [ $1 != 0 ]; then
+# Download something from the web and save it locally.
+# For example: 'download https://www.gnu.org/software/bash/manual/bash.txt'
+download () {
+  test -z $1 && exit 1
+  SAVE_AS=$(basename $1)
+  wget -O $SAVE_AS $1
+  if [ $? -ne 0 ]; then
     echo "Something went wrong. Are you connected to the internet?"
+    exit 2
   fi
 }
 
-wget -O install_ansible.sh https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/install_ansible.sh
-check_download_error $?
-
-wget -O configure_dev_workstation.yml https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/configure_dev_workstation.yml
-check_download_error $?
-
-bash install_ansible.sh
-
-ansible-playbook configure_dev_workstation.yml
+# Install differently depending on if system has a GUI desktop or not.
+DEFAULT_TARGET=$(systemctl get-default)
+if [ "$DEFAULT_TARGET" == "graphical.target" ]; then
+  echo "Configuring for interactive desktop use."
+  download https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/install_ansible.sh
+  download https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/configure_gui_workstation.yml
+  bash ./install_ansible.sh
+  ansible-playbook ./configure_dev_workstation.yml
+elif [ "$DEFAULT_TARGET" == "multi-user.target" ]; then
+  echo "Configuring for web-based remote access."
+  download https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/install_ansible.sh
+  download https://github.com/DavesCodeMusings/school-coding-lab/raw/main/rpi/fleet/configure_web_workstation.yml
+  bash ./install_ansible.sh
+  ansible-playbook ./configure_web_workstation.yml
+else
+  echo "Unable to determine if system role is desktop or web-based."
+  exit 3
+fi
 
 echo -n "Everything is done but the reboot. Restart now [y/N]? "
 read REPLY
