5394fc28be2a3ed59231619ea5c3554c6bfc971f
Dave Horton <dhorton668@outlook.com>
2024-08-15 17:01:40 -0500

    Dynamic link update POC

---
+++ lab-guides/script/functions.js
@@ -0,0 +1,21 @@
+/**
+ * Recreate a link with a new hostname. This allows something like this:
+ *   <a href="http://127.0.0.1:8080/">127.0.0.1:8080</a>
+ * to become this:
+ *   <a href="http://192.168.1.100:8080/">192.168.1.100:8080</a>
+ * where the 192.168.1.100 part comes from what JavaScript determines is
+ * the host portion of the URL, and http, 8080, and / all come from
+ * function parameters.
+ * This is useful for web server hosts with changing IP addresses. 
+ * @param {string} id 
+ * @param {string} protocol 
+ * @param {integer} port 
+ * @param {string} path 
+ */
+function updateLink(id, protocol, port, path) {
+    if (!path) path = '/'
+    let url = protocol + '://' + window.location.host + ':' + port + path
+    console.log("New URL for", id, "is", url)
+    document.getElementById(id).href = url
+    document.getElementById(id).innerHTML = url
+}
