952dc97e51f7a4a3e7a13b973611203d7c7b0b21
Dave Horton <dhorton668@outlook.com>
2024-08-18 20:05:32 -0500

    Update to match lab guide

--- micropython/neopixel/neopixel.py
+++ micropython/neopixel/neopixel.py
@@ -1,6 +1,14 @@
 from machine import Pin
 from neopixel import NeoPixel
+from time import sleep
 
-np = NeoPixel(Pin(8), 1)
-np[0] = (255, 0, 0)
-np.write()
+walk_signal = NeoPixel(Pin(8), 1)
+
+while True:
+    walk_signal[0] = (255, 0, 0)
+    walk_signal.write()
+    sleep(5)
+
+    walk_signal[0] = (255, 255, 255)
+    walk_signal.write()
+    sleep(5)
